a00a73dca1e96a98ec16fb8596644700d066e204
Oleg Sidorenko <sidorenko6503@gmail.com>
2025-01-31 15:24:59 +0300

    getHello method added to appservice

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,10 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  // async getHello(): Promise<User[]> {
-  //   return await AppDataSource
-  //     .getRepository(User)
-  //     .createQueryBuilder('user')
-  //     .getMany();
-  // }
+    getHello(): string {
+        return "Hello";
+  }
 }
