2ecf7a377d3853051716d38302353b19375b7e67
Oleg Sidorenko <sidorenko6503@gmail.com>
2025-03-21 21:17:31 +0300

    added separation for ports for local and server development

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
+  await app.listen(5000); // for local hosting
+  // await app.listen(5000);
 }
 
 
