d6831589bfbde8f8621cc5f1eda33b65d8536e2c
Oleg Sidorenko <sidorenko6503@gmail.com>
2025-03-26 17:21:39 +0300

    added missing files

---
+++ src/module/module.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ModuleService } from './module.service';
+import { ModuleConfig } from '../types/Module/module';
+import { Public } from '../decorators/isPublic.decorator';
+
+@Controller('module')
+export class ModuleController {
+  constructor(private readonly moduleService: ModuleService) {}
+
+  @Public()
+  @Get()
+  getModules(): ModuleConfig[] {
+    return this.moduleService.getModules();
+  }
+
+  @Public()
+  @Post('enable')
+  enableModule(@Body() moduleName: string) {
+    return this.moduleService.enableModule(moduleName);
+  }
+}
---
+++ src/module/module.service.ts
@@ -0,0 +1,91 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { CreateModuleDto } from './dto/create-module.dto';
+import { UpdateModuleDto } from './dto/update-module.dto';
+import * as fs from 'node:fs';
+import { ModuleConfig, ModulesFile } from '../types/Module/module';
+import * as path from 'node:path';
+import { URL } from '../helpers/env';
+import { exec } from 'child_process';
+
+
+const CONFIG_PATH = path.join(__dirname, '../../configs/modules.json');
+
+
+@Injectable()
+export class ModuleService implements OnModuleInit {
+  private modules: ModuleConfig[] = [];
+
+  constructor() {
+    this.loadModuleConfig();
+  }
+
+  async onModuleInit() {
+    await this.loadModuleConfig();
+  }
+
+  private async loadModuleConfig() {
+    try {
+      const fileContents = fs.readFileSync('configs/modules.json', 'utf8');
+      const parsedConfig = JSON.parse(fileContents) as ModulesFile;
+      this.modules = parsedConfig.modules || [];
+      // console.log('Modules loaded:', this.modules);
+    } catch (error) {
+      this.modules = [];
+    }
+  }
+
+  private saveConfig() {
+    fs.writeFileSync(CONFIG_PATH, JSON.stringify(this.modules, null, 2), 'utf8');
+  }
+
+  getModules(): ModuleConfig[] {
+    return this.modules;
+  }
+
+  enableModule(name: string) {
+    const module = this.modules?.find(m => m.name === name);
+    if (module) {
+      module.enabled = true;
+      this.saveConfig();
+      this.updateNginx();
+    }
+  }
+
+  disableModule(name: string) {
+    const module = this.modules?.find(m => m.name === name);
+    if (module) {
+      module.enabled = false;
+      this.saveConfig();
+      this.updateNginx();
+    }
+  }
+
+  private updateNginx() {
+    const config = this.modules
+      .filter(m => m.enabled)
+      .map(m => `location /api/${m.name} proxy_pass http://${m.services.backend.host}:${m.services.backend.port}`)
+      .join('/n');
+
+    const nginxConfig = `
+      server {
+        listen 443 ssl;
+        server_name ${URL};
+
+        ssl_certificate /etc/nginx/ssl/nginx.crt;
+        ssl_certificate_key /etc/nginx/ssl/nginx.key;
+
+        location / {
+                proxy_pass http://127.0.0.1:5000;
+                proxy_set_header Host $host;
+                proxy_set_header X-Real-IP $remote_addr;
+                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        }
+      }
+    `;
+    fs.writeFileSync('/etc/nginx/sites-available/api.misaserver.ru', nginxConfig);
+
+    exec('systemctl restart nginx', (err) => {
+      // if (err) console.error('Ошибка при обновлении Nginx:', err);
+    });
+  }
+}
