3c891a8e4ec40bfa76920f10060d6c65699291de
Oleg Sidorenko <sidorenko6503@gmail.com>
2025-03-29 19:27:16 +0300

    deleted unnecessary code

--- src/module/module.service.ts
+++ src/module/module.service.ts
@@ -1,15 +1,10 @@
-import { BadRequestException, Injectable, OnModuleInit } from '@nestjs/common';
-import { CreateModuleDto } from './dto/create-module.dto';
-import { UpdateModuleDto } from './dto/update-module.dto';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import * as fs from 'node:fs';
-import { ModuleConfig, ModulesFile } from '../types/Module/module';
+import { ModuleConfig } from '../types/Module/module';
 import * as path from 'node:path';
-import { URL } from '../helpers/env';
-import { exec } from 'child_process';
 
 
 const CONFIG_PATH = path.join(__dirname, '../../../ModulesConfigs/modules.json');
-const NGINX_CONFIG_PATH = '/etc/nginx/sites-available/api.misaserver.ru';
 
 
 @Injectable()
@@ -29,7 +24,6 @@ export class ModuleService implements OnModuleInit {
       const fileContents = fs.readFileSync(CONFIG_PATH, 'utf8');
       const parsedConfig = JSON.parse(fileContents) as ModuleConfig[];
       this.modules = parsedConfig || [];
-      console.log('Modules loaded:', this.modules);
     } catch (error) {
       this.modules = [];
     }
@@ -45,11 +39,9 @@ export class ModuleService implements OnModuleInit {
 
   enableModule(name: string) {
     const module = this.modules?.find(m => m.name === name);
-    console.log(this.modules, name);
     if (module) {
       module.enabled = true;
       this.saveConfig();
-      this.updateNginx();
     }
   }
 
@@ -58,48 +50,6 @@ export class ModuleService implements OnModuleInit {
     if (module) {
       module.enabled = false;
       this.saveConfig();
-      this.updateNginx();
-    }
-  }
-
-  private updateNginx() {
-    try {
-      const config = this.modules
-        .filter(m => m.enabled)
-        .map(m => `
-          location /api/${m.name}/ {
-            proxy_pass http://${m.services.backend.host}:${m.services.backend.port};
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          }
-        `)
-        .join('\n');
-
-      const nginxConfig = `
-      server {
-        listen 443 ssl;
-        server_name api.misaserver.ru;
-
-        ssl_certificate /etc/nginx/ssl/nginx.crt;
-        ssl_certificate_key /etc/nginx/ssl/nginx.key;
-
-        ${config}
-      }
-      `;
-
-      fs.writeFileSync(NGINX_CONFIG_PATH, nginxConfig, 'utf8');
-      console.log('Nginx config updated');
-
-      exec('nginx -s reload', (err, stdout, stderr) => {
-        if (err) {
-          console.error('Error reloading Nginx:', stderr);
-        } else {
-          console.log('Nginx reloaded successfully');
-        }
-      });
-    } catch (err) {
-      throw new BadRequestException(err);
     }
   }
 }
